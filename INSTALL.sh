1123a9556bff5901e441ca49fa1649010ef1c655
joshroche1 <joshua.a.roche@gmail.com>
2020-12-19 12:38:12 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 # run as root
-# from Ubuntu 20.04
+# from Ubuntu 20.04 / Debian 10
 apt-get update
 apt-get upgrade -y
 apt-get install apache2 php ansible -y
-# AWS CLI
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
-# Azure CLI
-curl -sL https://aka.ms/InstallAzureCLIDeb | bash
-# GCP SDK
-RUN echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-sdk -y
-# OCI CLI
-bash -c "$(curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh)"
+
+mkdir -p /var/www/html/resources/appml_models
+mkdir -p /var/www/html/ansible/playbooks
+mkdir /var/www/html/ansible/logs
+cp *.php /var/www/html/
+cp -R resources/* /var/www/html/resources
+cp ansible/playbooks/test_local.yml /var/www/html/ansible/playbooks/test_local.yml
