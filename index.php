05fe86f66b8af151283051b2b58750e02a7573c0
joshroche1 <joshua.a.roche@gmail.com>
2021-01-09 19:39:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,58 @@
 <!DOCTYPE html>
 <head>
-  <title>Cloud Management Interface</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-  <link rel="stylesheet" href="../resources/w3.css">
-  <script src="../resources/jquery-3.5.1.min.js"></script>
+  <title> CMI </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta> 
+  <link rel="stylesheet" href="../resources/css/w3.css">
+  <script src="../resources/js/jquery-3.5.1.min.js"></script>
 </head>
-<body>
-
-<div class="w3-container w3-blue-grey">
-  <a href="../index.php">
-  <h1>Cloud Management Interface</h1></a>
+<body> 
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a href="/index" class="w3-bar-item w3-button w3-blue-gray">
+      CMI
+    </a>
+    <a href="/dashboard" class="w3-bar-item w3-button">
+      Dashboard
+    </a>
+    <a href="/servers" class="w3-bar-item w3-button">
+      Servers
+    </a>
+    <a href="/cloud" class="w3-bar-item w3-button">
+      Cloud
+    </a>
+    <a href="/hypervisors" class="w3-bar-item w3-button">
+      Hypervisors
+    </a>
+    <a href="/settings" class="w3-bar-item w3-button">
+      Settings
+    </a>
+  </div>
 </div>
 
-<div class="w3-bar w3-black w3-card">
-  <a href="../dashboard.php" class="w3-bar-item w3-button">Dashboard</a>
-  <a href="../servers.php" class="w3-bar-item w3-button">Servers</a>
-  <a href="../cloud.php" class="w3-bar-item w3-button">Cloud</a>
-  <a href="../hypervisors.php" class="w3-bar-item w3-button">Hypervisors</a>
-  <a href="../admin.php" class="w3-bar-item w3-button">Admin</a>
-  <a href="../jquery.html" class="w3-bar-item w3-button">jQuery</a>
-</div>
-
-<div class="w3-row-padding">
-
-<div class="w3-col s12 m5 l3 w3-container w3-blue w3-card" style="margin:5px;">
-<h2>Welcome to the Cloud Management Interface</h2>
-</div>
-
-<div class="w3-col s12 m5 l8 w3-container w3-blue w3-card" style="margin:5px;">
-<p>A web based console for simple management of servers, cloud compute instances, and hypervisors. Built on Apache 2, PHP, jQuery, SQLite 3, and Ansible.</p>
-</div>
-
-</div>
-
-</div>
-
-<div class="w3-container w3-blue-grey">
-<h2>Cloud Management Interface</h2>
-</div>
+<main>
+<div class = "w3-row w3-light-gray"> 
+  <div class = "w3-container w3-blue w3-card" style = "margin:5px;padding:5px;">
+    <h1> Cloud Management Interface </h1>
+    <div class = "w3-half w3-container" style = "margin:5px;">
+	  <div class = "w3-container w3-white" style = "padding:5px;">
+	    <h2>Description</h2>
+This system is designed to perform simple administrative tasks on servers deployed to cloud providers such as Amazon Web Services EC2 and Google Cloud Platform.
+It can also work with server instances run on several hypervisor systems such as Citrix XenServer and VMWare ESXi.
+	  </div>
+    </div>
+    <div class = "w3-half w3-container" style = "margin:5px;">
+	  <div class = "w3-container w3-white">
+	    <h2>Features:</h2>
+	    <ul>
+		<li>Web Console for Administration</li>
+		<li>Monitor server instances and restart/shutdown systems</li>
+		<li>Update server systems through native package management</li>
+		</ul>
+	  </div>
+    </div>
+  </div>
+</div> 
+</main>
 
 </body>
 </html>
